33a6abdc485f88b13f0187839d2813a862414509
DawnT0wn <756231955@qq.com>
2023-08-25 11:58:01 +0800

    update 2023-08-25

--- Java-Decompile-AllJar.py
+++ Java-Decompile-AllJar.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import subprocess
 import concurrent.futures
 import sys  # 导入 sys 模块以访问命令行参数
@@ -28,9 +29,28 @@ def process_jar(jar_file):
     target_dir = os.path.join(output_directory, jar_file_name)
     os.makedirs(target_dir, exist_ok=True)
 
-    # 解压 JAR 文件到目标目录
-    subprocess.run(["unzip", "-d", target_dir, jar_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # unizp 解压 JAR 文件到目标目录
+    # subprocess.run(["unzip", "-d", target_dir, jar_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
+    # jar 解压 复制 JAR 文件到目标目录
+    shutil.copy(jar_file, target_dir)
+
+    # 获取当前目录
+    original_directory = os.getcwd()
+
+    # 切换到 JAR 文件所在的目录
+    os.chdir(target_dir)
+
+    # 使用 jar 命令解压 JAR 文件到当前目录
+    result = subprocess.run(["jar", "-xvf", f"{jar_file_name}.jar"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if result.returncode != 0:
+        print(f"Error: {result.stderr.decode('utf-8')}")
+
+    # 返回到原始目录
+    os.chdir(original_directory)
+
+    # 删除copy的jar包
+    os.remove(os.path.join(target_dir, f"{jar_file_name}.jar"))
     # 构建反编译命令
     decompile_command = f"java -cp java-decompiler.jar org.jetbrains.java.decompiler.main.decompiler.ConsoleDecompiler -hdc=0 -dgs=1 -rsy=1 -rbr=1 -lit=1 -nls=1 -mpm=60 {target_dir} {target_dir}"
 
